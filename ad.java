c9079a142418c32cd5b58a38275f2522257af697
aritika <aritika2000@gmail.com>
2019-01-14 12:35:47 +0530

    file

---
+++ ad.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+	public class ad{
+	public static void main( String args[]){
+	int x,y,z;
+	System.out.println("Enter two nos to find there sum");
+	Scanner in = new Scanner(System.in);
+	x=in.nextInt();
+	y=in.nextInt();	
+	z=x+y;
+	System.out.println("Sum of the integers =" +z);
+	}
+	}
